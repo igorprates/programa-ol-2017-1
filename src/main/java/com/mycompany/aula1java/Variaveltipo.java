48605ca01d6710067ef03f210ade55b256fe693d
igorprates <igor.prates100@gmail.com>
2017-03-22 20:48:28 -0300

    Update Variaveltipo.java

--- src/main/java/com/mycompany/aula1java/Variaveltipo.java
+++ src/main/java/com/mycompany/aula1java/Variaveltipo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.estacio.terenciani.programacao;
+package com.mycompany.aula1java;
 
 /**
  *
