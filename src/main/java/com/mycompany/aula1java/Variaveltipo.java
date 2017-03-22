82a1842ead95594fe6f685a301a56fa369ce716b
igorprates <igor.prates100@gmail.com>
2017-03-22 20:47:37 -0300

    Update Variaveltipo.java

--- src/main/java/com/mycompany/aula1java/Variaveltipo.java
+++ src/main/java/com/mycompany/aula1java/Variaveltipo.java
@@ -3,19 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.aula1java;
+package br.estacio.terenciani.programacao;
 
 /**
  *
- * @author aluno
+ * @author Marcelo
  */
-public class Variaveltipo {
+public class VariavelTipo {
 
     /**
      * @param args the command line arguments
      */
+    
+   
+     
     public static void main(String[] args) {
-        // TODO code application logic here
+        //imprime();
+        //variavel inteira chamada "x" recebe o valor 10
+        int x = 10;
+        //variavel inteira chamada "y" recebe o valor 4
+        int y = 4;
+        
+        int resultado;
+        
+        resultado = multiplica(x,y);
+        
+        System.out.println("O resultado eh: " + resultado);
+        
     }
+    
+    public static int multiplica (int x, int y){
+        return x * y;
+    }
+    
+     public static void imprime(){
+        System.out.println("-----");
+        System.out.println("Teste");
+        System.out.println("-----");
+    }
+   
     
 }
